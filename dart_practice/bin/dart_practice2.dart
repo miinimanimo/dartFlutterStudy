f8520712befb32a1ce605efc634ec26f0594813f
miinimanimo <ssk07079@gmail.com>
2025-05-02 17:42:54 +0900

    String interpolation

--- dart_practice/bin/dart_practice2.dart
+++ dart_practice/bin/dart_practice2.dart
@@ -1,3 +1,4 @@
+// 자료형
 // void main() {
 //   String name = 'nico';
 //   bool isAwesome = true;
@@ -8,25 +9,33 @@
 // }
 
 //list
+// void main() {
+//   var giveMeFive = true;
+//   var numbers = [
+//     1,
+//     2,
+//     3,
+//     4,
+//     // if (giveMeFive) 5,
+//   ];
+
+//   // if (giveMeFive) 5, <-이거랑 동일한 의미로 밑의 if문
+//   if (giveMeFive) {
+//     numbers.add(5);
+//   }
+
+//   print(numbers);
+
+//   // List<int> numbers2 = [1, 2, 3, 4, 5,];
+//   // numbers.add(1);
+//   // print(numbers.first);
+//   // print(numbers);
+// }
+
+//String interpolation
 void main() {
-  var giveMeFive = true;
-  var numbers = [
-    1,
-    2,
-    3,
-    4,
-    // if (giveMeFive) 5,
-  ];
-
-  // if (giveMeFive) 5, <-이거랑 동일한 의미로 밑의 if문
-  if (giveMeFive) {
-    numbers.add(5);
-  }
-
-  print(numbers);
-
-  // List<int> numbers2 = [1, 2, 3, 4, 5,];
-  // numbers.add(1);
-  // print(numbers.first);
-  // print(numbers);
+  var name = 'nico';
+  var age = 12;
+  var greeting = 'Hello, my name is $name and I\'m ${age + 2}';
+  print(greeting);
 }
