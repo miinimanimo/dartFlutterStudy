2a879e0d445c55c39888b705a95afce13f647372
miinimanimo <ssk07079@gmail.com>
2025-05-04 05:38:38 +0900

    named constructor

--- dart_practice/bin/dart_practice3.dart
+++ dart_practice/bin/dart_practice3.dart
@@ -44,11 +44,36 @@
 // }
 
 //named constructor parameter
+// class Player {
+//   final String name;
+//   int xp;
+//   String team;
+//   int age;
+
+//   Player({
+//     required this.name,
+//     required this.xp,
+//     required this.team,
+//     required this.age,
+//   });
+
+//   void sayHello() {
+//     print('Hi my name is $name');
+//   }
+// }
+
+// void main() {
+//   var player = Player(name: "nico", xp: 1500, team: "red", age: 20);
+//   player.sayHello();
+//   var player2 = Player(name: "lynn", xp: 1200, team: "blue", age: 21);
+//   player2.sayHello();
+// }
+
+//named constructor
 class Player {
   final String name;
-  int xp;
+  int xp, age;
   String team;
-  int age;
 
   Player({
     required this.name,
